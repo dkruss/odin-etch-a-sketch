05e9b1bbb3b155cb25552a00a4a831df626a9363
David Kruss <davidis1337@gmail.com>
2025-05-12 18:08:46 +1000

    Add grid size and colour options

--- javascript.js
+++ javascript.js
@@ -1,19 +1,75 @@
 let container = document.querySelector(".container");
+let setSizeBtn = document.querySelector("#setSize");
 
-for (let c = 0; c < 16; c++) {
-    let column = document.createElement("div");
-    for (let r = 0; r < 16; r++) {
-        let row = document.createElement("div");
-        row.classList.add("square");
-        column.append(row);
+let mouseOverColour = document.querySelector("#mouseOverColour");
+let clickColour = document.querySelector("#clickColour");
+let doubleClickColour = document.querySelector("#doubleClickColour");
+
+mouseOverColour.value = "#ff0000"
+clickColour.value = "#00ff00"
+doubleClickColour.value = "#0000ff"
+
+function createGrid(s) {
+    
+    //If a grid already exists when creating a new one, 
+    //clear old one first
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
-    container.append(column);
+
+    //Create grid
+    for (let r = 0; r < s; r++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let c = 0; c < s; c++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.append(cell);
+        }
+        container.append(row);
+    }
+    
+    const cells = document.querySelectorAll(".cell");
+    
+    cells.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style["background-color"] = mouseOverColour.value;
+        });
+        div.addEventListener("click", () => {
+            div.style["background-color"] = clickColour.value;
+        });
+        div.addEventListener("dblclick", () => {
+            div.style["background-color"] = doubleClickColour.value;
+        });
+        div.addEventListener("contextmenu", function (e) {
+            e.preventDefault();
+            div.style["background-color"] = "";
+        });
+    });
+
+    mouseOverColour.addEventListener("input", () => {
+        mouseOverColour = document.querySelector("#mouseOverColour");
+    });
+    clickColour.addEventListener("input", () => {
+        clickColour = document.querySelector("#clickColour");
+    });
+    doubleClickColour.addEventListener("input", () => {
+        doubleClickColour = document.querySelector("#doubleClickColour");
+    });
 }
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((div) => {
-    div.addEventListener("click", () => {
-        div.style["background-color"] = "red";
-    });
+setSizeBtn.addEventListener("click", () => {
+    let size = prompt("How large should the grid be?");
+    if (size > 0 && size <= 100) {
+        createGrid(size);
+    } else if (size > 100) {
+        alert("Size is too large. Setting grid size to 16.");
+        createGrid(16);
+    } else {
+        alert("Invalid input. Setting grid size to 16.");
+        createGrid(16);
+    }
 });
+
+
+createGrid(16);
